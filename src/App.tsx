fb6b86a971560a97daf02454a2c39c7d89f86844
Ashiqur Rahman <imashiqe@gmail.com>
2024-08-30 14:50:00 +0600

    car wash booking system front end

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-green-400">
+      <h1>React App</h1>
+      <p>React App with TypeScript</p>
+    </div>
+  );
+}
+
+export default App;
