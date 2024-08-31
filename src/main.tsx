0939f1cf5561b0e95dad450530fc81dd3ae318e3
Ashiqur Rahman <imashiqe@gmail.com>
2024-08-31 11:47:31 +0600

    added header footer

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "../src/redux/feature/store.ts";
+import Header from "../src/Shared/Header/Header.jsx";
+import Footer from "../src/Shared/Footer/Footer.jsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
+      <Header></Header>
       <App />
+      <Footer></Footer>
     </Provider>
   </StrictMode>
 );
